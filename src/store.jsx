c7e32eb885ebd879a1bbd18615fc66c855103f78
unknown <muktinathrajbanshi786@gmail.com>
2025-12-03 08:16:12 +0545

    wow same logic with different style i.e toolkit

---
+++ src/store.jsx
@@ -0,0 +1,96 @@
+// import { applyMiddleware, createStore } from "redux";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+// import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
+
+/* eslint-disable no-case-declarations */
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
+const FETCH_TASKS = "task/fetch";
+
+
+const initialState = {
+    task: [],
+    isLoading: false,
+};
+
+// Step 5: Create action creators
+export const addTask = (data) => {
+   return { type: ADD_TASK, payload: data }
+}; 
+
+export const deleteTask = (id) => {
+    return { type: DELETE_TASK, payload: id }
+};
+
+export const fetchTask = () => {
+    return async (dispatch) => {
+        try {
+            const res = await fetch(
+                "https://jsonplaceholder.typicode.com/todos?_limit=3"
+            );
+            const task = await res.json();
+            console.log(task);
+            
+            dispatch({ 
+                type: FETCH_TASKS, 
+                payload: task.map((curTask) => curTask.title),
+             });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+const taskReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TASK:
+            return {
+                ... state,
+                task: [... state.task, action.payload],
+            };
+
+        case DELETE_TASK:
+            const updatedTask = state.task.filter((curTask, index) => index !== action.payload); 
+               return {
+                ...state,
+                task: updatedTask,
+               };
+
+        case FETCH_TASKS: 
+            return {
+                ...state,
+                task:[... state.task, ...action.payload],
+            }       
+        default:
+            return state;
+    }
+};
+
+//! Step 2: Create the Redux store using the reducer 
+// export const store = createStore(
+//     taskReducer, 
+//     composeWithDevTools(applyMiddleware(thunk))
+// );
+
+//! New Style
+export const store = configureStore({
+    reducer: {
+        taskReducer,
+    },
+});
+
+// Step 3: Log the initial state 
+console.log("initial State: ", store.getState());
+
+// Step 4: Dispatch an action to add a task 
+store.dispatch(addTask("Good Morning!"));
+store.dispatch(addTask("Good Afternoon!"));
+store.dispatch(addTask("Good Night"));
+console.log("updated State: ", store.getState());
+
+
+
+
+
+
